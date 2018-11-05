9b08bfc3af0199ba6a4e7986270c3f1f344a771c
renz <denkide@users.noreply.github.com>
2018-11-05 09:14:42 -0800

    Update dbconnection.js

--- NodeDemo/dbconnection.js
+++ NodeDemo/dbconnection.js
@@ -2,7 +2,7 @@ var mysql=require('mysql');
 var connection=mysql.createPool({
     host:'localhost',
     user:'root',
-    password:'root',
+    password:'xxxxx',
     database:'thunderbox_WPSP',
     multipleStatements: true
 });
